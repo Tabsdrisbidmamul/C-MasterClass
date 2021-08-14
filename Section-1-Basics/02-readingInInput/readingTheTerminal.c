d25987377ee64e6ccaf07abda471ae02491b6243
Idris Khan <idris.khan303@gmail.com>
2021-08-14 16:08:00 +0100

    renamed the project strucutre

---
+++ Section-1-Basics/02-readingInInput/readingTheTerminal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+  char text[100];
+  int i;
+
+  printf("Enter a value: ");
+  scanf("%s %d", text, &i);
+
+  printf("\nYou entered: %s %d", text, i);
+  
+  return 0;
+}
